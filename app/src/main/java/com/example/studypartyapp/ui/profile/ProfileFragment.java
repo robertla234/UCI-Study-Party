57f53e7ebc85f4695eac9b4b0dab066365a34676
robertla234 <robertla234@yahoo.com>
2021-01-22 19:34:36 -0800

    Update ProfileFragment.java

--- app/src/main/java/com/example/studypartyapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/studypartyapp/ui/profile/ProfileFragment.java
@@ -166,7 +166,7 @@ public class ProfileFragment extends Fragment {
         //  "partyID": 123, "class": "Testing", "size": 3, "purpose": "Testing", "location": "Online", "meetTime": 23, "hostID": 8
         //Output Format:
         //  CLASS PURPOSE at TIME \n at LOCATION
-        String nameInputParse = nameInput.substring(23);
+        nameInput = nameInput.substring(0, nameInput.indexOf(" \"guests\":"));
         String[] output = new String[2];
 
         String[] keyValuePairs = nameInput.split(",");
